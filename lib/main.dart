b4748ade606f7165ea6ddcf52045a355588a587a
Thinuka20 <140114175+Thinuka20@users.noreply.github.com>
2024-12-04 16:22:48 +0530

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:genix_reports/pages/login.dart';
-import 'package:genix_reports/pages/newdashboard.dart';
+import 'package:genix_reports/pages/dashboard.dart';
 import 'package:get/get.dart';
 
 
